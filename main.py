f6487bbbddce44ce6ce3e902161c28357010fb7e
Vitaliy <121188492+VitaliySidorov@users.noreply.github.com>
2023-06-21 12:04:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os, re
 
 def phone_format(n):  # форматирование телефонного номера
     n = n.removeprefix("+")
-    n = n.removeprefix("1")
     n = re.sub("[ ()-]", "", n)
     return format(int(n[:-1]), ",").replace(",", "-") + n[-1]
 
